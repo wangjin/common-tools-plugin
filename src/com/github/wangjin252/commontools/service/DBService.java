eeb66317edb244e73389e0406fbee4bb303235f6
wangjin <wangjin-252@hotmail.com>
2018-08-15 16:56:02 +0800

    获取数据库元信息

--- src/com/github/wangjin252/commontools/service/DBService.java
+++ src/com/github/wangjin252/commontools/service/DBService.java
@@ -1,8 +1,11 @@
 package com.github.wangjin252.commontools.service;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import com.github.wangjin252.commontools.entity.Column;
+import com.github.wangjin252.commontools.entity.Table;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBService {
 
@@ -12,4 +15,41 @@ public class DBService {
         String url = String.format("jdbc:mysql://%s:%d/%s?useUnicode=true&characterEncoding=UTF-8&useSSL=false&connectTimeout=60000&socketTimeout=60000&autoReconnect=true&autoReconnectForPools=true&failOverReadOnly=false", ip, port, dbName);
         return DriverManager.getConnection(url, userName, password);
     }
+
+    public static List<Table> getTables(Connection connection) throws SQLException {
+        DatabaseMetaData metaData = connection.getMetaData();
+        // 获取表结果集
+        ResultSet tableResultSet = metaData.getTables(null, "%", "%", null);
+
+        List<Table> tables = new ArrayList<>();
+        while (tableResultSet.next()) {
+            Table table = new Table();
+            table.setName(tableResultSet.getString("TABLE_NAME"));
+            table.setColumns(getColumns(metaData, table.getName()));
+
+            tables.add(table);
+        }
+
+        return tables;
+    }
+
+    private static List<Column> getColumns(DatabaseMetaData metaData, String tableName) throws SQLException {
+        List<Column> columns = new ArrayList<>();
+        ResultSet resultSet = metaData.getColumns(null, "%", tableName, "%");
+        while (resultSet.next()) {
+            Column column = new Column();
+            column.setName(resultSet.getString("COLUMN_NAME"));
+            column.setType(resultSet.getString("TYPE_NAME"));
+            columns.add(column);
+        }
+        return columns;
+    }
+
+
+    public static void close(Connection connection) throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+
 }
